c8f180a0673ed158bcf0d7db08be92b3f2477304
Nikolai Maslak <nikomaslak@gmail.com>
2026-05-25 10:58:46 -0700

    quiz1

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -10,7 +10,22 @@
 
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(quantity: u32) -> u32 {
+    const BASE_PRICE: u32 = 2;
+    const DISCOUNTED_PRICE: u32 = 1;
+    const DISCOUNT_QUANTITY: u32 = 40;
+
+    let price = if quantity > DISCOUNT_QUANTITY {
+        DISCOUNTED_PRICE
+    } else {
+        BASE_PRICE
+    };
+
+    let cost = price * quantity;
+    println!("{price}, {quantity}, {cost}");
+
+    cost
+}
 
 fn main() {
     // You can optionally experiment here.
