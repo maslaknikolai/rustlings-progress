5d0abbaa1ea595906d0ff4a45e1bd96062617dd9
Nikolai Maslak <nikomaslak@gmail.com>
2026-06-04 22:42:54 -0700

    iterators4

--- exercises/18_iterators/iterators4.rs
+++ exercises/18_iterators/iterators4.rs
@@ -10,6 +10,14 @@ fn factorial(num: u64) -> u64 {
     // - additional variables
     // For an extra challenge, don't use:
     // - recursion
+
+    let mut r = 1;
+
+    for i in 2..=num {
+        r *= i;
+    }
+
+    r
 }
 
 fn main() {
